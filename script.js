446ac8ab77ac90fa9809f80ad91d8935622bae6e
Collin Stone <collin.stone@compassdigital.io>
2024-06-24 13:14:37 -0400

    commit two

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ document
     const video = document.getElementById("video");
 
     try {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: { facingMode: "user" },
+      });
       video.srcObject = stream;
       dialog.style.display = "block";
     } catch (error) {
+      alert("Error accessing camera: " + error.message);
       console.error("Error accessing camera: ", error);
     }
   });
